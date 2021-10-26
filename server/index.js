6532052e67ea7e61f4ffcbfcada30fe449ff5a89
Don de Rojas <derojasdonny@gmail.com>
2021-10-25 19:35:43 -0700

    feat: connected fcted basic frontend to rest api

--- server/index.js
+++ server/index.js
@@ -1,22 +1,29 @@
 import express from 'express';
 import fetch from 'node-fetch';
+import cors from 'cors';
+
 
 const app = express();
+app.use(cors())
 const port = 3001
 const baseURL = `https://api.github.com`; 
 // https://api.github.com/repos/dderojas/Todo-App/pulls/1/commits
 // data.length for the array of commits should do this
 app.get('/', (req, res) => {
-  console.log(req.query, 'testttttt')
-// fetch(`${baseURL}/repos/dderojas/Todo-App/pulls`)
-//   .then((data) => {
-//     console.log(data, 'asdfasdfasdf')
-//     return data.json()
-//   })
-//   .then((data) => {
-//     console.log(data?.length, 'dataaaaa')
-//     res.status(200).send(data)
-//   })
+  const { gitURL } = req.query;
+  let userAndRepo = gitURL.split('/')
+  
+  fetch(`${baseURL}/repos/${userAndRepo[1]}/${userAndRepo[2]}/pulls`)
+    .then((data) => {
+      return data.json()
+    })
+    .then((data) => {
+      console.log(data?.length, 'dataaaaa')
+      let results = data.map((item) => {
+        return item.title
+      })
+      res.status(200).send(results)
+    })
   
 })
 
--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-// import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const { value } = e.target.children[0];
+    
+    const params = {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      mode: 'cors',
+      cache: 'default'
+    }
+
+    fetch(`http://localhost:3001/?gitURL=${value}`, params)
+    .then((data) => {
+      return data.json()
+    })
+    .then((data) => {
+      console.log(data, 'data!!!')
+      setItems([...data, items])
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         Github Api Node App
       </header>
-      <input type="text"></input>
-      <button>send</button>
+      <form onSubmit={handleSubmit}>
+        <input type="text"></input>
+        <button>send</button>
+      </form>
+      {items.map((value) => {
+        return <div>{value}</div>
+      })}
     </div>
   );
 }
